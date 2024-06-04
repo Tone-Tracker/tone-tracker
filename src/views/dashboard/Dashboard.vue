c682e3ceb1b953a3d2612515a4fd72204ba1b7eb
Mazisi Msebele <mazisimsebele18@gmail.com>
2024-06-04 13:45:24 +0200

    Dashboard -WIP

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -3,167 +3,189 @@
 		<!--start page wrapper -->
 		<div class="page-wrapper">
 			<div class="page-content">
-				<div class="row row-cols-1 row-cols-md-2 row-cols-xl-4">
-                   <div class="col">
-					 <div class="card radius-10 border-start border-0 border-4 border-info">
-						<div class="card-body">
-							<div class="d-flex align-items-center">
-								<div>
-									<p class="mb-0 text-secondary">Total Orders</p>
-									<h4 class="my-1 text-info">4805</h4>
-									<p class="mb-0 font-13">+2.5% from last week</p>
-								</div>
-								<div class="widgets-icons-2 rounded-circle bg-gradient-blues text-white ms-auto"><i class='bx bxs-cart'></i>
-								</div>
-							</div>
-						</div>
-					 </div>
-				   </div>
-				   <div class="col">
-					<div class="card radius-10 border-start border-0 border-4 border-danger">
-					   <div class="card-body">
-						   <div class="d-flex align-items-center">
-							   <div>
-								   <p class="mb-0 text-secondary">Total Revenue</p>
-								   <h4 class="my-1 text-danger">$84,245</h4>
-								   <p class="mb-0 font-13">+5.4% from last week</p>
-							   </div>
-							   <div class="widgets-icons-2 rounded-circle bg-gradient-burning text-white ms-auto"><i class='bx bxs-wallet'></i>
-							   </div>
-						   </div>
-					   </div>
-					</div>
-				  </div>
-				  <div class="col">
-					<div class="card radius-10 border-start border-0 border-4 border-success">
-					   <div class="card-body">
-						   <div class="d-flex align-items-center">
-							   <div>
-								   <p class="mb-0 text-secondary">Bounce Rate</p>
-								   <h4 class="my-1 text-success">34.6%</h4>
-								   <p class="mb-0 font-13">-4.5% from last week</p>
-							   </div>
-							   <div class="widgets-icons-2 rounded-circle bg-gradient-ohhappiness text-white ms-auto"><i class='bx bxs-bar-chart-alt-2' ></i>
-							   </div>
-						   </div>
-					   </div>
-					</div>
-				  </div>
-				  <div class="col">
-					<div class="card radius-10 border-start border-0 border-4 border-warning">
-					   <div class="card-body">
-						   <div class="d-flex align-items-center">
-							   <div>
-								   <p class="mb-0 text-secondary">Total Customers</p>
-								   <h4 class="my-1 text-warning">8.4K</h4>
-								   <p class="mb-0 font-13">+8.4% from last week</p>
-							   </div>
-							   <div class="widgets-icons-2 rounded-circle bg-gradient-orange text-white ms-auto"><i class='bx bxs-group'></i>
-							   </div>
-						   </div>
-					   </div>
-					</div>
-				  </div> 
-				</div><!--end row-->
 
-				<div class="row">
-                   <div class="col-12 col-lg-8 d-flex">
-                      <div class="card radius-10 w-100">
-						<div class="card-header">
-							<div class="d-flex align-items-center">
-								<div>
-									<h6 class="mb-0">Sales Overview</h6>
-								</div>
-								<div class="dropdown ms-auto">
-									<a class="dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown"><i class='bx bx-dots-horizontal-rounded font-22 text-option'></i>
-									</a>
-									<ul class="dropdown-menu">
-										<li><a class="dropdown-item" href="javascript:;">Action</a>
-										</li>
-										<li><a class="dropdown-item" href="javascript:;">Another action</a>
-										</li>
-										<li>
-											<hr class="dropdown-divider">
-										</li>
-										<li><a class="dropdown-item" href="javascript:;">Something else here</a>
-										</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-						  <div class="card-body">
-							<div class="d-flex align-items-center ms-auto font-13 gap-2 mb-3">
-								<span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #14abef"></i>Sales</span>
-								<span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #ffc107"></i>Visits</span>
-							</div>
-							<div class="chart-container-1">
-								<canvas id="chart1"></canvas>
-							  </div>
-						  </div>
-						  <div class="row row-cols-1 row-cols-md-3 row-cols-xl-3 g-0 row-group text-center border-top">
-							<div class="col">
-							  <div class="p-3">
-								<h5 class="mb-0">24.15M</h5>
-								<small class="mb-0">Overall Visitor <span> <i class="bx bx-up-arrow-alt align-middle"></i> 2.43%</span></small>
-							  </div>
-							</div>
-							<div class="col">
-							  <div class="p-3">
-								<h5 class="mb-0">12:38</h5>
-								<small class="mb-0">Visitor Duration <span> <i class="bx bx-up-arrow-alt align-middle"></i> 12.65%</span></small>
-							  </div>
-							</div>
-							<div class="col">
-							  <div class="p-3">
-								<h5 class="mb-0">639.82</h5>
-								<small class="mb-0">Pages/Visit <span> <i class="bx bx-up-arrow-alt align-middle"></i> 5.62%</span></small>
-							  </div>
-							</div>
-						  </div>
-					  </div>
-				   </div>
-				   <div class="col-12 col-lg-4 d-flex">
+                <div class="row">
+                    <div class="col-12 col-lg-12 d-flex">
                        <div class="card radius-10 w-100">
-						<div class="card-header">
-							<div class="d-flex align-items-center">
-								<div>
-									<h6 class="mb-0">Trending Products</h6>
-								</div>
-								<div class="dropdown ms-auto">
-									<a class="dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown"><i class='bx bx-dots-horizontal-rounded font-22 text-option'></i>
-									</a>
-									<ul class="dropdown-menu">
-										<li><a class="dropdown-item" href="javascript:;">Action</a>
-										</li>
-										<li><a class="dropdown-item" href="javascript:;">Another action</a>
-										</li>
-										<li>
-											<hr class="dropdown-divider">
-										</li>
-										<li><a class="dropdown-item" href="javascript:;">Something else here</a>
-										</li>
-									</ul>
+                         <div class="card-header">
+                             <div class="d-flex align-items-center">
+                                 <div>
+                                     <h6 class="mb-0">Sales Overview</h6>
+                                 </div>
+                                 <div class="dropdown ms-auto">
+                                     <a class="dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown"><i class='bx bx-dots-horizontal-rounded font-22 text-option'></i>
+                                     </a>
+                                     <ul class="dropdown-menu">
+                                         <li><a class="dropdown-item" href="javascript:;">Action</a>
+                                         </li>
+                                         <li><a class="dropdown-item" href="javascript:;">Another action</a>
+                                         </li>
+                                         <li>
+                                             <hr class="dropdown-divider">
+                                         </li>
+                                         <li><a class="dropdown-item" href="javascript:;">Something else here</a>
+                                         </li>
+                                     </ul>
+                                 </div>
+                             </div>
+                         </div>
+                           <div class="card-body">
+                             <div class="d-flex align-items-center ms-auto font-13 gap-2 mb-3">
+                                 <span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #14abef"></i>Sales</span>
+                                 <span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #ffc107"></i>Visits</span>
+                             </div>
+                             <div class="chart-container-1">
+                                 <canvas id="chart1"></canvas>
+                               </div>
+                           </div>
+                           <div class="row row-cols-1 row-cols-md-3 row-cols-xl-3 g-0 row-group text-center border-top">
+                             <div class="col">
+                               <div class="p-3">
+                                 <h5 class="mb-0">24.15M</h5>
+                                 <small class="mb-0">Overall Visitor <span> <i class="bx bx-up-arrow-alt align-middle"></i> 2.43%</span></small>
+                               </div>
+                             </div>
+                             <div class="col">
+                               <div class="p-3">
+                                 <h5 class="mb-0">12:38</h5>
+                                 <small class="mb-0">Visitor Duration <span> <i class="bx bx-up-arrow-alt align-middle"></i> 12.65%</span></small>
+                               </div>
+                             </div>
+                             <div class="col">
+                               <div class="p-3">
+                                 <h5 class="mb-0">639.82</h5>
+                                 <small class="mb-0">Pages/Visit <span> <i class="bx bx-up-arrow-alt align-middle"></i> 5.62%</span></small>
+                               </div>
+                             </div>
+                           </div>
+                       </div>
+                    </div>
+                  
+                 </div>
+
+
+				<div class="row row-cols-1 row-cols-md-3 row-cols-xl-4">
+					<div class="col">
+						<div class="card radius-10">
+							<div class="card-body">
+								<div class="text-center">
+									<div class="widgets-icons rounded-circle mx-auto bg-light-primary text-primary mb-3"><i class='bx bxl-facebook-square'></i>
+									</div>
+									<h4 class="my-1 maz-height">84</h4>
+									<p class="mb-0 text-secondary">Activation Process</p>
 								</div>
 							</div>
 						</div>
-						   <div class="card-body">
-							<div class="chart-container-2">
-								<canvas id="chart2"></canvas>
-							  </div>
-						   </div>
-						   <ul class="list-group list-group-flush">
-							<li class="list-group-item d-flex bg-transparent justify-content-between align-items-center border-top">Jeans <span class="badge bg-success rounded-pill">25</span>
-							</li>
-							<li class="list-group-item d-flex bg-transparent justify-content-between align-items-center">T-Shirts <span class="badge bg-danger rounded-pill">10</span>
-							</li>
-							<li class="list-group-item d-flex bg-transparent justify-content-between align-items-center">Shoes <span class="badge bg-primary rounded-pill">65</span>
-							</li>
-							<li class="list-group-item d-flex bg-transparent justify-content-between align-items-center">Lingerie <span class="badge bg-warning text-dark rounded-pill">14</span>
-							</li>
-						</ul>
-					   </div>
-				   </div>
-				</div><!--end row-->
+					</div>
+					<div class="col">
+						<div class="card radius-10">
+							<div class="card-body">
+								<div class="text-center">
+									<div class="widgets-icons rounded-circle mx-auto bg-light-danger text-danger mb-3"><i class='bx bxl-twitter'></i>
+									</div>
+									<p class="mb-0 text-secondary">Leads</p>
+									<h4 class="my-1 maz-height">34</h4>
+									
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col">
+						<div class="card radius-10">
+							<div class="card-body">
+								<div class="text-center">
+									<div class="widgets-icons rounded-circle mx-auto bg-light-info text-info mb-3"><i class='bx bxl-linkedin-square'></i>
+									</div>
+									<p class="mb-0 text-secondary">Activations Completed</p>
+									<h4 class="my-1 maz-height">56</h4>
+									
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col">
+						<div class="card radius-10">
+							<div class="card-body">
+								<div class="text-center">
+									<div class="widgets-icons mx-auto  mb-3"><i class='bx bx-line-chart'></i>
+									</div>
+									<p class="mb-0 text-secondary">Ongoing Activations</p>
+									<h4 class="my-1 maz-height">38</h4>
+									
+								</div>
+							</div>
+						</div>
+					</div>
+				
+				</div>
+
+				<div class="row row-cols-1 row-cols-md-3 row-cols-xl-3">
+				
+					<div class="col">
+						<div class="card radius-10 overflow-hidden">
+							<div class="card-body">
+								<div class="d-flex align-items-center">
+									<div>
+										<p class="mb-0">Total Revenue</p>
+										<h5 class="mb-0 invisible">Actual: R0.00</h5>
+										<h5 class="mb-0 text-success">86%</h5>
+									</div>
+									<div class="ms-auto">	<h5 class="mb-0">Actual: R0.00</h5>
+										<h5 class="mb-0 mt-4 float-end">Plan: R0.00</h5>
+									</div>
+								</div>
+								<div class="progress radius-10 mt-4" style="height:25px;">
+									<div class="progress-bar bg-success" role="progressbar" style="width: 76%"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					
+					<div class="col">
+						<div class="card radius-10 overflow-hidden">
+							<div class="card-body">
+								<div class="d-flex align-items-center">
+									<div>
+										<p class="mb-0">Total Cost</p>
+										<h5 class="mb-0 invisible">Actual: R0.00</h5>
+										<h5 class="mb-0 text-danger">86%</h5>
+									</div>
+									<div class="ms-auto">	<h5 class="mb-0">Actual: R0.00</h5>
+										<h5 class="mb-0 mt-4 float-end">Plan: R0.00</h5>
+									</div>
+								</div>
+								<div class="progress radius-10 mt-4" style="height:25px;">
+									<div class="progress-bar bg-danger" role="progressbar" style="width: 76%"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					
+					<div class="col">
+						<div class="card radius-10 overflow-hidden">
+							<div class="card-body">
+								<div class="d-flex align-items-center">
+									<div>
+										<p class="mb-0">Total Margin</p>
+										<h5 class="mb-0 invisible">Actual: R0.00</h5>
+										<h5 class="mb-0 text-primary">86%</h5>
+									</div>
+									<div class="ms-auto">	<h5 class="mb-0">Actual: R0.00</h5>
+										<h5 class="mb-0 mt-4 float-end">Plan: R0.00</h5>
+									</div>
+								</div>
+								<div class="progress radius-10 mt-4" style="height:25px;">
+									<div class="progress-bar bg-primary" role="progressbar" style="width: 76%"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				
+				</div>
+
+				
 
 				 <div class="card radius-10">
 					<div class="card-header">
@@ -516,102 +538,14 @@
 			</div>
 		</div>
 	<!--start switcher-->
-	<div class="switcher-wrapper">
-		<div class="switcher-btn"> <i class='bx bx-cog bx-spin'></i>
-		</div>
-		<div class="switcher-body">
-			<div class="d-flex align-items-center">
-				<h5 class="mb-0 text-uppercase">Theme Customizer</h5>
-				<button type="button" class="btn-close ms-auto close-switcher" aria-label="Close"></button>
-			</div>
-			<hr/>
-			<h6 class="mb-0">Theme Styles</h6>
-			<hr/>
-			<div class="d-flex align-items-center justify-content-between">
-				<div class="form-check">
-					<input class="form-check-input" type="radio" name="flexRadioDefault" id="lightmode" checked>
-					<label class="form-check-label" for="lightmode">Light</label>
-				</div>
-				<div class="form-check">
-					<input class="form-check-input" type="radio" name="flexRadioDefault" id="darkmode">
-					<label class="form-check-label" for="darkmode">Dark</label>
-				</div>
-				<div class="form-check">
-					<input class="form-check-input" type="radio" name="flexRadioDefault" id="semidark">
-					<label class="form-check-label" for="semidark">Semi Dark</label>
-				</div>
-			</div>
-			<hr/>
-			<div class="form-check">
-				<input class="form-check-input" type="radio" id="minimaltheme" name="flexRadioDefault">
-				<label class="form-check-label" for="minimaltheme">Minimal Theme</label>
-			</div>
-			<hr/>
-			<h6 class="mb-0">Header Colors</h6>
-			<hr/>
-			<div class="header-colors-indigators">
-				<div class="row row-cols-auto g-3">
-					<div class="col">
-						<div class="indigator headercolor1" id="headercolor1"></div>
-					</div>
-					<div class="col">
-						<div class="indigator headercolor2" id="headercolor2"></div>
-					</div>
-					<div class="col">
-						<div class="indigator headercolor3" id="headercolor3"></div>
-					</div>
-					<div class="col">
-						<div class="indigator headercolor4" id="headercolor4"></div>
-					</div>
-					<div class="col">
-						<div class="indigator headercolor5" id="headercolor5"></div>
-					</div>
-					<div class="col">
-						<div class="indigator headercolor6" id="headercolor6"></div>
-					</div>
-					<div class="col">
-						<div class="indigator headercolor7" id="headercolor7"></div>
-					</div>
-					<div class="col">
-						<div class="indigator headercolor8" id="headercolor8"></div>
-					</div>
-				</div>
-			</div>
-			<hr/>
-			<h6 class="mb-0">Sidebar Colors</h6>
-			<hr/>
-			<div class="header-colors-indigators">
-				<div class="row row-cols-auto g-3">
-					<div class="col">
-						<div class="indigator sidebarcolor1" id="sidebarcolor1"></div>
-					</div>
-					<div class="col">
-						<div class="indigator sidebarcolor2" id="sidebarcolor2"></div>
-					</div>
-					<div class="col">
-						<div class="indigator sidebarcolor3" id="sidebarcolor3"></div>
-					</div>
-					<div class="col">
-						<div class="indigator sidebarcolor4" id="sidebarcolor4"></div>
-					</div>
-					<div class="col">
-						<div class="indigator sidebarcolor5" id="sidebarcolor5"></div>
-					</div>
-					<div class="col">
-						<div class="indigator sidebarcolor6" id="sidebarcolor6"></div>
-					</div>
-					<div class="col">
-						<div class="indigator sidebarcolor7" id="sidebarcolor7"></div>
-					</div>
-					<div class="col">
-						<div class="indigator sidebarcolor8" id="sidebarcolor8"></div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+	
 </Layout>
 </template>
 <script setup>
 import Layout from '../shared/Layout.vue';
 </script>
+<style>
+.maz-height{
+	font-size: 3rem;
+}
+</style>
