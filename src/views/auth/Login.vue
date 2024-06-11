c5000d61e5fee56f4291272007030d299eea4bbf
David Mwakajumba <mwakajumbadavid@gmail.com>
2024-06-11 11:11:27 +0200

    fix login page

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -2,73 +2,85 @@
 import { RouterLink, RouterView } from 'vue-router'
 </script>
 <template>
+	<!-- <img class="logo-light" src="../../assets/images/logo/white-logo.png" alt="logo">
+	<img class="shoes" src="../../assets/images/login-images/Shoes.png" alt=""> -->
+	<div class="logo-light"></div>
+	<div class="shoes" </div>
+		<div class="container-login">
 
-	<div class="wrapper">
-		<div class="section-authentication-cover">
-			<div class="">
-				<div class="row g-0">
+			<div class="section-authentication-cover">
 
-					<div
-						class="col-12 col-xl-7 col-xxl-8 auth-cover-left align-items-center justify-content-center d-none d-xl-flex">
+				<div class="login-cover">
+					<div class="row g-0">
 
-						<div class="card shadow-none bg-transparent shadow-none rounded-0 mb-0">
-							<div class="card-body">
+						<div class="col-12 col-xl-7 col-xxl-8 auth-cover-left align-items-center d-none d-xl-flex">
 
-							</div>
-						</div>
-
-					</div>
-
-					<div class="col-12 col-xl-5 col-xxl-4 auth-cover-right align-items-center justify-content-center">
-						<div class="card rounded-0 m-3 shadow-none bg-transparent mb-0">
-							<div class="card-body p-sm-5">
-								<div class="">
-
-									<div class="form-body">
-										<h5 class="text-default">Login</h5>
-										<form class="row g-3">
-											<div class="col-12">
-												<label for="inputEmailAddress" class="form-label">Email</label>
-												<input type="email" class="form-control custom-input"
-													id="inputEmailAddress" placeholder="jhon@example.com">
-											</div>
-											<div class="col-12">
-												<label for="inputChoosePassword" class="form-label">Password</label>
-												<div class="input-group" id="show_hide_password">
-													<input type="password" class="form-control border-end-0"
-														id="inputChoosePassword" value="12345678"
-														placeholder="Enter Password">
-
-
-												</div>
-											</div>
-
-											<div class="col-12">
-												<div class="d-grid">
-													<router-link to="/dashboard" type="submit"
-														class="btn maz-gradient-btn">Continue</router-link>
-												</div>
-											</div>
-
-										</form>
-									</div>
+							<div class="card shadow-none bg-transparent shadow-none rounded-0 mb-0">
+								<div class="card-body">
 
 								</div>
 							</div>
-						</div>
-					</div>
 
+						</div>
+
+						<div
+							class="col-12 col-xl-5 col-xxl-4 auth-cover-right align-items-center justify-content-center">
+							<div class="card rounded-0 m-3 shadow-none bg-transparent mb-0">
+								<div class="card-body p-sm-5">
+									<div class="">
+
+										<div class="form-body">
+											<h5 class="mb-3 text-default">Log in</h5>
+											<form class="row g-3">
+												<div class="mb-3 col-12">
+													<label for="inputEmailAddress" class="form-label">User</label>
+													<input type="email" class="form-control custom-input"
+														id="inputEmailAddress" placeholder="">
+												</div>
+												<div class="mb-5 col-12">
+													<label for="inputChoosePassword" class="form-label">Password</label>
+													<div class="input-group" id="show_hide_password">
+														<input type="password" class="form-control border-end-0"
+															id="inputChoosePassword" value="12345678" placeholder="">
+
+
+													</div>
+												</div>
+
+												<div class="col-12">
+													<div class="d-grid">
+														<router-link to="/dashboard" type="submit"
+															class="btn p-3 maz-gradient-btn">Continue</router-link>
+													</div>
+												</div>
+
+											</form>
+										</div>
+
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+					<!--end row-->
 				</div>
-				<!--end row-->
 			</div>
 		</div>
-	</div>
-	<RouterView />
+		<RouterView />
 </template>
 <style scoped>
-.auth-cover-right {
-	background: rgba(18, 24, 26, 0.7);
-	opacity: .9;
+.text-default {
+	font-size: 40px;
+	font-weight: 200;
+	color: #fff
+}
+
+.form-label {
+	font-size: 20px;
+	font-weight: 200;
+	margin-bottom: 0;
+	color: #fff;
 }
 
 input[type=email] {
@@ -104,16 +116,57 @@ input[type=password]:focus {
 }
 
 .auth-cover-left {
+	position: relative;
 	background-color: transparent;
 }
 
+.logo-light {
+	position: absolute;
+	height: 69px;
+	width: 315px;
+	background-image: url("../../assets/images/logo/white-logo.png");
+	top: 20px;
+	left: 40px;
+}
+
+.shoes {
+	position: absolute;
+	height: 108px;
+	width: 105px;
+	background-image: url("../../assets/images/login-images/Shoes.png");
+	background-repeat: no-repeat;
+	bottom: 20px;
+	left: 40px
+}
+
+
+
 .form-control {
 	border-radius: 0;
+	background-color: transparent !important;
+	padding: 0;
 }
 
 .form-control:hover {
 	border: none;
 	box-shadow: none;
 	border-bottom: 2px solid #fff;
+}
+
+.auth-cover-right {
+	background: rgb(34, 36, 35, 0.7);
+	border-left: 1px solid #707070;
+}
+
+@media (max-width: 768px) {
+	.shoes {
+		display: none;
+	}
+}
+
+@media (max-width: 575.98px) {
+	.shoes {
+		display: none;
+	}
 }
 </style>
