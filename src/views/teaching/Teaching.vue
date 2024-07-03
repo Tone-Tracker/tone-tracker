fca8f901fc13d4e7ddb1c059cd64913f6ffd638a
Mazisi Msebele <56942052+mazisi@users.noreply.github.com>
2024-07-03 23:34:22 +0200

    Few Fixes

--- src/views/teaching/Teaching.vue
+++ src/views/teaching/Teaching.vue
@@ -16,15 +16,13 @@ import BreadCrumb from '@/components/BreadCrumb.vue';
 
                 <div class="card">
                     <div class="card-body">
-                        <!-- Code here -->
-                        <div class="container">
                             <div>
                             </div>
                             <div class="row">
-                                <div v-for="i in 5" class="col-md-4 col-lg-3">
+                                <div v-for="i in 5" class="col-md-3 col-lg-2">
                                     <div class="brief-card mb-4">
-                                        <h5 class="py-1 px-4 mb-0 text-center bg-black">Joburg Activation 28 Februarv
-                                        </h5>
+                                        <h5 class="py-1 px-4 mb-0 text-center bg-black">Joburg Activation 28 Februar</h5>
+                                        <img src="https://www.iconpacks.net/icons/1/free-document-icon-901-thumb.png" class="bg-white">
                                         <div class="d-flex justify-content-between align-items-center">
                                             <button
                                                 class="btn bg-black text-white w-100 rounded-0 btn-outline-light">Read</button>
@@ -35,7 +33,7 @@ import BreadCrumb from '@/components/BreadCrumb.vue';
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                       
                     </div>
                 </div>
             </div>
@@ -43,9 +41,8 @@ import BreadCrumb from '@/components/BreadCrumb.vue';
     </Layout>
 </template>
 <style scoped>
-body {
-    background-color: #1c1c1c;
-    color: white;
+bg-white {
+    background-color: #dedede;
 }
 
 .bg-black {
