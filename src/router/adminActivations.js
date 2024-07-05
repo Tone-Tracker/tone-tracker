f6af7b357cc02e5f1517139420db4a7c5ddf4dc8
David Mwakajumba <mwakajumbadavid@gmail.com>
2024-07-05 14:32:24 +0200

    add admin activation

---
+++ src/router/adminActivations.js
@@ -0,0 +1,8 @@
+import AdminActivations from '../views/admin/AdminActivations.vue';
+export default [
+        {
+                path: '/adminActivations',
+                name: 'adminActivations',
+                component: AdminActivations,
+        }
+]
