79d0690781158b62f496d640982e2175e7b16bc7
David Mwakajumba <mwakajumbadavid@gmail.com>
2024-07-11 09:23:21 +0200

    add header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,114 +1,184 @@
 <template>
-    <header>
-        <div class="topbar d-flex align-items-center">
-            <nav class="navbar navbar-expand-lg flex justify-content-between flex-direction-row w-100">
-                <div class="mobile-toggle-menu d-lg-none"><i class='bx bx-menu'></i>
-                </div>
-                <div class="logo-container">
-                    <img class="logo" src="/src/assets/images/logo/white-logo.png" alt="Logo">
-                </div>
-
-                <div class="search-bar d-none d-lg-flex w-100 justify-content-center">
-                    <div class="input-group custom-width-70">
-                        <span class="input-group-text" id="addon-wrapping"><i class='bx bx-search'></i></span>
-                        <input type="text" class="form-control" aria-label="Text input with 2 dropdown buttons"
-                            placeholder="Search For Projects...">
-                        <button class="btn btn-outline-secondary w-20 maz-gradient-btn">Search</button>
-                    </div>
-                </div>
-
-                <div class="user-box dropdown px-3">
-                    <a class="d-flex align-items-center nav-link gap-3" href="#" role="button">
-                        <img src="/src/assets/images/logo/Ellipse.png" class="user-img" alt="user avatar">
-                    </a>
-                </div>
-            </nav>
+  <header>
+    <div class="topbar d-flex align-items-center">
+      <nav class="navbar navbar-expand-lg flex justify-content-between flex-direction-row w-100">
+        <div class="mobile-toggle-menu d-lg-none">
+          <i class='bx bx-menu'></i>
         </div>
-    </header>
+        <div class="logo-container">
+          <img class="logo" src="/src/assets/images/logo/white-logo.png" alt="Logo">
+        </div>
+
+        <div class="search-bar-container d-none d-lg-flex w-100 justify-content-center">
+          <div class="input-group custom-width-70">
+            <span class="input-group-text" id="addon-wrapping"><i class='bx bx-search'></i></span>
+            <input
+              type="text"
+              class="form-control"
+              aria-label="Text input with dropdown suggestions"
+              placeholder="Search For Projects..." 
+              v-model="query"
+              @input="onInput"
+            />
+            <button class="btn btn-outline-secondary w-20 maz-gradient-btn">Search</button>
+            <ul v-if="filteredSuggestions.length" class="suggestions-list">
+              <li
+                v-for="(suggestion, index) in filteredSuggestions"
+                :key="index"
+                @click="selectSuggestion(suggestion)"
+              >
+                {{ suggestion }}
+              </li>
+          </ul>
+          </div>
+          
+        </div>
+
+        <div class="user-box dropdown px-3">
+          <a class="d-flex align-items-center nav-link gap-3" href="#" role="button">
+            <img src="/src/assets/images/logo/Ellipse.png" class="user-img" alt="user avatar">
+          </a>
+        </div>
+      </nav>
+    </div>
+  </header>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      query: '',
+      suggestions: ['Project Alpha', 'Project Beta', 'Project Gamma', 'Project Delta'],
+      filteredSuggestions: []
+    };
+  },
+  methods: {
+    onInput() {
+      if (this.query.length > 0) {
+        this.filteredSuggestions = this.suggestions.filter(suggestion =>
+          suggestion.toLowerCase().includes(this.query.toLowerCase())
+        );
+      } else {
+        this.filteredSuggestions = [];
+      }
+    },
+    selectSuggestion(suggestion) {
+      this.query = suggestion;
+      this.filteredSuggestions = [];
+    }
+  }
+};
+</script>
 
 <style scoped>
 .form-control {
-    background-color: #1C1C1C !important;
-    border-left: 0 !important;
-    border-top-left-radius: 0;
-    border: 1px solid #000000 !important;
-    text-align: center;
-    color: white !important;
+  background-color: #1C1C1C !important;
+  border-left: 0 !important;
+  border-top-left-radius: 0;
+  border: 1px solid #000000 !important;
+  text-align: center;
+  color: white !important;
 }
 
 .form-control::placeholder {
-    text-align: center;
-    color: white !important;
+  text-align: center;
+  color: white !important;
 }
 
 .form-control:focus {
-    border-left: 0;
-    outline: 0;
+  border-left: 0;
+  outline: 0;
 }
 
-.search-bar {
-    cursor: pointer;
+.search-bar-container {
+  cursor: pointer;
+  position: relative;
 }
 
 .custom-width-70 {
-    width: 70%;
+  width: 70%;
 }
 
 .user-img {
-    width: 58px;
-    height: 58px;
-    border: none !important;
+  width: 58px;
+  height: 58px;
+  border: none !important;
 }
 
 html.dark-theme .user-box {
-    border-left: 0px solid rgb(255 255 255 / .15);
-    border-right: 0px solid rgb(255 255 255 / .15);
+  border-left: 0px solid rgb(255 255 255 / .15);
+  border-right: 0px solid rgb(255 255 255 / .15);
 }
 
 html.dark-theme .input-group-text {
-    color: #d1d7de;
-    background-color: #1C1C1C !important;
-    border: 1px solid #000000;
-    border-radius: 0;
+  color: #d1d7de;
+  background-color: #1C1C1C !important;
+  border: 1px solid #000000;
+  border-radius: 0;
 }
 
 .btn {
-    border-radius: 0;
+  border-radius: 0;
 }
 
 .logo-container {
-    flex-grow: 1;
-    display: flex;
-    justify-content: center;
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
 }
 
 .logo {
-    width: 180px;
+  width: 180px;
+}
+
+.suggestions-list {
+  position: absolute;
+  top: 100%;
+  left: 43px;
+  width: 592.05px;
+  background-color: #1C1C1C;
+  border: 1px solid #000000;
+  z-index: 1000;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  max-height: 200px;
+  overflow-y: auto;
+}
+
+.suggestions-list li {
+  padding: 10px;
+  cursor: pointer;
+  color: white;
+}
+
+.suggestions-list li:hover {
+  background-color: #2A2A2A;
 }
 
 @media (max-width: 991.98px) {
-    .search-bar {
-        display: none !important;
-    }
-    .custom-width-70 {
-        width: 100%;
-    }
+  .search-bar-container {
+    display: none !important;
+  }
+  .custom-width-70 {
+    width: 100%;
+  }
 }
 
 @media (max-width: 767.98px) {
-    .logo-container {
-        justify-content: flex-start;
-    }
-    .user-box {
-        order: -1;
-    }
-    .navbar {
-        flex-direction: column;
-        align-items: start;
-    }
-    .logo {
-        width: 150px;
-    }
+  .logo-container {
+    justify-content: flex-start;
+  }
+  .user-box {
+    order: -1;
+  }
+  .navbar {
+    flex-direction: column;
+    align-items: start;
+  }
+  .logo {
+    width: 150px;
+  }
 }
 </style>
---
+++ src/views/adminActivation/AdminActivations.vue
@@ -0,0 +1,147 @@
+<script setup>
+import { onMounted, ref, reactive } from 'vue';
+import Layout from '@/views/shared/Layout.vue';
+import BreadCrumb from '@/components/BreadCrumb.vue';
+import UsersModal from './UsersModal.vue';
+import { useUserStore } from '@/stores/userStore';
+import useToaster from '@/composables/useToaster';
+
+
+const userStore = useUserStore();
+const toaster = useToaster();
+let users = ref([]);
+let showLoading = ref(false);
+let modalData = reactive({});
+
+onMounted(() => {
+	getAllUsers();
+})
+
+let showModal = ref(true);
+const toggleModal = () => {
+	showModal.value = true,
+	modalData.value = {}
+}
+
+const hideModal = () => {
+	showModal.value = false,
+	getAllUsers();
+}
+
+    const searchInput = ref('');
+
+const onInput = () => {
+	  console.log('searchInput',searchInput.value, users.value.content)
+	 if(searchInput.value) {
+		// return users.value = users.value.content.filter(user => user.firstName.toLowerCase().includes(searchInput.value.toLowerCase()) 
+		// || user.lastName.toLowerCase().includes(searchInput.value.toLowerCase()))
+	 }else{
+		getAllUsers();
+	 }
+  };
+
+  const getAllUsers = () => {
+	showLoading.value = true;
+	userStore.getUsers().then(function (response) {
+		showLoading.value = false;
+		users.value = response.data
+	}).catch(function (error) {
+		toaster.error("Error fetching users");
+		console.log(error);
+	}).finally(function () {
+		showLoading.value = false;
+	})
+  }
+
+  const showDetails = (user) => {
+	modalData.value = user;
+  }
+
+  const deleteUser = (user) => {
+	if(confirm("Are you sure you want to delete this user?")) {
+		userStore.deleteUser(user.id).then(function (response) {
+		toaster.success("User deleted successfully");
+		//refetch data
+		getAllUsers();
+	   }).catch(function (error) {
+		toaster.error("Error deleting user");
+		console.log(error);
+	   })	
+  }
+}
+
+</script>
+<template>
+    <Layout>
+        <div class="page-wrapper">
+			<div class="page-content">
+                <BreadCrumb title="All Users" icon="bx bxs-user-badge"/>
+				<div class="card">
+					<div class="card-body">
+						<div class="d-lg-flex align-items-center mb-4 gap-3">
+							<div class="position-relative">
+								<input v-model="searchInput" @input="onInput"
+								type="text" class="form-control ps-5 radius-30" placeholder="Search"> <span class="position-absolute top-50 product-show translate-middle-y"><i class="bx bx-search"></i></span>
+							</div>
+						  <div class="ms-auto">
+							<a @click="toggleModal" href="javascript:;" data-bs-toggle="modal" data-bs-target="#create-user"  class="btn maz-gradient-btn radius-30 mt-2 mt-lg-0">
+							<i class="bx bxs-plus-square"></i>Add User</a></div>
+						</div>
+						<div class="table-responsive">
+							<table class="table mb-0">
+								<thead class="table-dark">
+									<tr>
+										<th>Contact Name</th>
+										<th>Contact Surname</th>
+										<th>Email</th>
+										<th>Cell Number</th>
+										<th>Opt In</th>
+										<th>Activation Area</th>
+										<th>Actions</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr v-if="users?.content" v-for="user in users?.content" :key="user.id">
+										<td>{{user.firstName}}</td>
+										<td>{{user.lastName}}</td>
+										<td>{{user.email}}</td>
+										<td>{{user.phone}}</td>
+										<td>
+											<div class="badge rounded-pill text-success bg-light-success p-2 text-uppercase px-3"><i class='bx bxs-circle align-middle me-1'></i>
+											Yes</div></td>
+											<td>
+												Johannesburg
+											</td>
+										<td>
+											<div class="d-flex order-actions">
+												<a @click="showDetails(user)" href="javascript:;" data-bs-toggle="modal" data-bs-target="#create-user" class="">
+													<i class='bx bxs-edit'></i></a>
+												<a @click="deleteUser(user)" href="javascript:;" class="ms-3"><i class='bx bxs-trash'></i></a>
+											</div>
+										</td>
+									</tr>
+									<tr v-else>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td colspan="7" class="text-center">
+											<!-- <CustomSpinner v-if="showLoading"/> -->
+										</td>
+									</tr>
+									
+								
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+
+				<UsersModal v-if="showModal"
+				:showModal="showModal"
+				:modalData="modalData"
+				@closeModal="hideModal()"
+				/>
+			</div>
+		</div>
+    </Layout>
+</template>
