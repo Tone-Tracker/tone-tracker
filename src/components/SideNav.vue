5be83607aa07a701a1d83f401f4ec82012ed0a2b
Mazisi Msebele <56942052+mazisi@users.noreply.github.com>
2024-08-02 13:58:10 +0200

    Dark Mode

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -43,9 +43,9 @@ const getUserRole = (role) => {
             <div class="content">
                 <nav class="sidebar d-flex">
                      <!-- <AdminNav/> -->
-                    <!-- <TTG_SUPER_ADMIN v-if="getUserRole('TTG_SUPER_ADMIN')" :user="user"/>
+                   <TTG_SUPER_ADMIN v-if="getUserRole('TTG_SUPER_ADMIN')" :user="user"/>
                     <TTG_REGIONAL_MANAGER v-if="getUserRole('TTG_REGIONAL_MANAGER')" :user="user"/>
-                    <TTG_HEAD_ADMIN v-if="getUserRole('TTG_HEAD_ADMIN')" :user="user"/> -->
+                    <TTG_HEAD_ADMIN v-if="getUserRole('TTG_HEAD_ADMIN')" :user="user"/>
                     <CLIENT v-if="getUserRole('TTG_SUPER_ADMIN')" :user="user" style="z-index: 9999"/>
                     <TTG_SUPER_ADMIN v-if="getUserRole('TTG_SUPER_ADMIN')" :user="user"/>
                     <TTG_ACTIVATION_MANAGER v-if="getUserRole('TTG_ACTIVATION_MANAGER')" :user="user"/>
