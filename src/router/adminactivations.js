a3eb96cab153cf8250f6c66acc17abc00933ffe5
Mazisi Msebele <56942052+mazisi@users.noreply.github.com>
2024-08-03 14:10:00 +0200

    Upload activation images

--- src/router/adminactivations.js
+++ src/router/adminactivations.js
@@ -1,3 +1,4 @@
+import ViewActivation from '@/views/activations/ViewActivation.vue';
 import AdminActivations from '../views/activations/AdminActivations.vue';
 import CreateActivation from '../views/activations/CreateActivation.vue';
 export default [
@@ -10,5 +11,10 @@ export default [
                 path: '/create-activation',
                 name: 'create-activation',
                 component: CreateActivation,
+        },
+        {
+                path: '/view-activation',
+                name: 'create-activation',
+                component: ViewActivation,
         }
-]
+];
