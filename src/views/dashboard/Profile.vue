983886e55ea58daea75ea28eb0e2a15bdce3e9ba
David Mwakajumba <mwakajumbadavid@gmail.com>
2024-08-15 14:15:13 +0200

    adjust edit icon  o profile

--- src/views/dashboard/Profile.vue
+++ src/views/dashboard/Profile.vue
@@ -692,6 +692,7 @@ div.desc {
 
 
 .image-container {
+    width: 300px;
     position: relative;
     display: inline-block;
 }
@@ -699,7 +700,7 @@ div.desc {
 .edit-icon {
     position: absolute;
     top: 3px;
-    right: 58px;
+    right: 3px;
     border-radius: 50%;      
     padding: 5px;  
     cursor: pointer;
