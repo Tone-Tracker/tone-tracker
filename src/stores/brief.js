e9eb23b472564e93456be33309ad7590398b70a5
riconyathi <riconyathi@gmail.com>
2024-09-29 20:42:34 +0200

    Update brief.js

--- src/stores/brief.js
+++ src/stores/brief.js
@@ -7,9 +7,26 @@ export const useBrief = defineStore('brief', () => {
       return axiosInstance.post(`/api/briefs`,formData, config);
     }
 
-    const getBriefs = (userRole, activeUserId) => {
-      return axiosInstance.get(`/api/briefs`);
-    }
+    
+
+    const getBriefs = (userRole, id = null) => {
+      console.log(id);
+      switch (userRole) {
+        case 'TTG_REGIONAL_MANAGER':
+          return axiosInstance.get(`/api/activations/brief/region/${id}`);
+        case 'TTG_ACTIVATION_MANAGER':
+          return axiosInstance.get(`/api/activations/brief/staff/${id}`);
+        case 'TTG_TALENT':
+          return axiosInstance.get(`/api/activations/brief/promoter/${id}`);
+        case 'SUPPLIER':
+          return axiosInstance.get(`/api/activations/brief/thirdParty/${id}`);
+        case 'CLIENT':
+          return axiosInstance.get(`/api/activations/brief/client/${id}`);
+        default:
+          return axiosInstance.get(`/api/briefs`);
+      }
+    };
+    
 
     const update = (id,form) => {
       return axiosInstance.put(`/api/briefs/${id}`,form);
