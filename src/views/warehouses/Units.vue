06fd213941a40a4fc8c41e5e7b2015120ea6094d
Mazisi Msebele <56942052+mazisi@users.noreply.github.com>
2024-10-08 18:01:52 +0200

    Update Units.vue

--- src/views/warehouses/Units.vue
+++ src/views/warehouses/Units.vue
@@ -77,8 +77,6 @@ const onUnitChange = (event) => {
 	unitName.value = warehouse.value?.unitsList?.find((unit) => unit.id == event.target.value)[0]?.name;
 	viewedUnit.value= null;
 	viewedUnit.value = warehouse.value?.unitsList?.find((unit) => unit.id == event.target.value);
-	// unitVisible.value = true; // Show the modal after unit selection
-console.log(viewedUnit.value)
 	unitForm.name = viewedUnit.value?.name;
 	unitForm.capacity = viewedUnit.value?.capacity;
 	unitForm.warehouse = viewedUnit.value?.warehouse;
