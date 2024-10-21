b5b54eefddd7640cc79c905f827816de0f3c08e4
Mazisi Msebele <56942052+mazisi@users.noreply.github.com>
2024-10-21 21:31:10 +0200

    Add text-muted to inactive lists

--- src/components/navigations/TTG_SUPER_ADMIN.vue
+++ src/components/navigations/TTG_SUPER_ADMIN.vue
@@ -34,25 +34,25 @@ const getRoleName = () => {
           <ul class="nav-list">
             <li class="nav-item" :class="{ 'active': currentRoute.path === '/budget' }">
               <router-link to="/budget">
-                <i class="icon bx bx-wallet"></i>
+                <i class="icon bx bx-wallet" :class="{ 'text-muted': currentRoute.path !== '/budget' }"></i>
                 <span>BUDGET</span>
               </router-link>
             </li>
             <li class="nav-item" :class="{ 'active': currentRoute.path === '/crm' }">
               <router-link to="/crm">
-                <i class="icon bx bx-grid-alt"></i>
+                <i class="icon bx bx-grid-alt" :class="{ 'text-muted': currentRoute.path !== '/crm' }"></i>
                 <span>CRM</span>
               </router-link>
             </li>
             <li class="nav-item" :class="{ 'active': currentRoute.path === '/status' }">
               <router-link to="/status">
-                <i class="icon bx bx-line-chart"></i>
+                <i class="icon bx bx-line-chart" :class="{ 'text-muted': currentRoute.path !== '/status' }" ></i>
                 <span>Campaign Status</span>
               </router-link>
             </li>
             <li class="nav-item" :class="{ 'active': currentRoute.path === '/clients' }">
               <router-link to="/clients">
-                <i class="icon bx bx-purchase-tag"></i>
+                <i class="icon bx bx-purchase-tag" :class="{ 'text-muted': currentRoute.path !== '/clients' }"></i>
                 <span>Brands</span>
               </router-link>
             </li>
@@ -63,19 +63,19 @@ const getRoleName = () => {
           <ul class="nav-list">
             <li class="nav-item" :class="{ 'active': currentRoute.path === '/report' }">
               <router-link to="/report">
-                <i class="icon bx bx-bar-chart-alt-2"></i>
+                <i class="icon bx bx-bar-chart-alt-2" :class="{ 'text-muted': currentRoute.path !== '/report' }"></i>
                 <span>Activations Report</span>
               </router-link>
             </li>
             <li class="nav-item" :class="{ 'active': currentRoute.path === '/activations' }">
               <router-link to="/activations">
-                <i class="icon bx bx-calendar-event"></i>
+                <i class="icon bx bx-calendar-event" :class="{ 'text-muted': currentRoute.path !== '/activations' }"></i>
                 <span>Live Activations</span>
               </router-link>
             </li>
             <li class="nav-item" :class="{ 'active': currentRoute.path === '/briefs' }">
               <router-link to="/briefs">
-                <i class="icon bx bx-file"></i>
+                <i class="icon bx bx-file" :class="{ 'text-muted': currentRoute.path !== '/briefs' }" ></i>
                 <span>Briefs</span>
               </router-link>
             </li>
@@ -86,31 +86,31 @@ const getRoleName = () => {
           <ul class="nav-list">
             <li class="nav-item" :class="{ 'active': currentRoute.path === '/warehouse' }">
               <router-link to="/warehouse">
-                <i class="icon bx bx-package"></i>
+                <i class="icon bx bx-package" :class="{ 'text-muted': currentRoute.path !== '/warehouse' }"></i>
                 <span>Warehouse</span>
               </router-link>
             </li>
             <li class="nav-item" :class="{ 'active': currentRoute.path === '/staff' }">
               <router-link to="/staff">
-                <i class="icon bx bx-user"></i>
+                <i class="icon bx bx-user" :class="{ 'text-muted': currentRoute.path !== '/staff' }"></i>
                 <span>Staff</span>
               </router-link>
             </li>
             <li class="nav-item" :class="{ 'active': currentRoute.path === '/promoters' }">
               <router-link to="/promoters">
-                <i class="icon bx bx-group"></i>
+                <i class="icon bx bx-group" :class="{ 'text-muted': currentRoute.path !== '/promoters' }"></i>
                 <span>Promoters</span>
               </router-link>
             </li>
             <li class="nav-item" :class="{ 'active': currentRoute.path === '/suppliers' }">
               <router-link to="/suppliers">
-                <i class="icon bx bxs-user-detail"></i>
+                <i class="icon bx bxs-user-detail" :class="{ 'text-muted': currentRoute.path !== '/suppliers' }"></i>
                 <span>Suppliers</span>
               </router-link>
             </li>
             <li class="nav-item" :class="{ 'active': currentRoute.path === '/downloads' }">
               <router-link to="/downloads">
-                <i class="icon bx bx-download"></i>
+                <i class="icon bx bx-download" :class="{ 'text-muted': currentRoute.path !== '/downloads' }"></i>
                 <span>Downloads</span>
               </router-link>
             </li>
