64071e5b18e177ad26c12f7b5882563035db3dcd
David Mwakajumba <mwakajumbadavid@gmail.com>
2024-10-23 13:18:42 +0200

    update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,40 @@
 <template>
     <footer class="page-footer ">
-        <p class="mb-0">Copyright © {{ new Date().getFullYear() }}. All right reserved.
-    </p>
+        <div></div>
+        <p class="mb-0 text-white">ToneTracker a TwoTone Global product. Copyright © {{ new Date().getFullYear() }}. All right reserved.
+        </p>
+        <!-- <div>
+            <div>FAQ's</div>
+            <div>Contact Us</div>
+            <div>Support</div> -->
+
+            <ul class="nav-list d-flex gap-2">
+                <a class="" href="#">FAQ's</a>
+                <a href="#">Contact Us</a>
+                <a href="#">Support</a>
+            </ul>
+        
     <!-- <span @click="logout" class=" btn text-white  maz-gradient-btn ml-4 float-end cursor-pointer">Logout</span> -->
     </footer>
 </template>
+
+<style scoped>
+    .page-footer {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr; 
+        align-items: center;
+    }
+    .nav-list {
+        margin: 0;
+        padding-left: 300px;
+    }
+
+    .nav-list a {
+        border-right: 2px solid #fff;
+        padding-right: 5px; 
+    }
+    .nav-list a:last-child {
+        border: 0;
+    }
+
+</style>
